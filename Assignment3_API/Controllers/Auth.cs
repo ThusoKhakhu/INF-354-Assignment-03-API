5006ea47713fbf78545c0d3e47445834d53e8aab
Mr. T Khakhu <u22586522@up.ac.za>
2025-05-19 11:03:54 +0200

    Add project files excluding .vs folder

---
+++ Assignment3_API/Controllers/Auth.cs
@@ -0,0 +1,203 @@
+﻿using Assignment3_API.Models;
+using Assignment3_API.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment3_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IRepository _repository;
+        private readonly IUserClaimsPrincipalFactory<AppUser> _claimsPrincipalFactory;
+        private readonly IConfiguration _configuration;
+
+
+        public Auth(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, IUserClaimsPrincipalFactory<AppUser> claimsPrincipalFactory, IConfiguration configuration, IRepository repository)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _claimsPrincipalFactory = claimsPrincipalFactory;
+            _configuration = configuration;
+            _repository = repository;
+        }
+
+        //Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(UserViewModel uvm)
+        {
+            var user = await _userManager.FindByIdAsync(uvm.emailaddress);
+
+            if (user == null)
+            {
+                user = new AppUser
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = uvm.emailaddress,
+                    Email = uvm.emailaddress
+                };
+
+                var result = await _userManager.CreateAsync(user, uvm.password);
+
+                if (result.Errors.Count() > 0)
+                {
+                    var errors = result.Errors.Select(e => e.Description).ToList();
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Registration failed", Errors = errors });
+                }
+            
+            }
+            else
+            {
+                return Forbid("Account already exists.");
+            }
+
+            return Ok();
+        }
+
+
+        //Login
+        [HttpPost]
+        [Route("Login")]
+        public async Task<ActionResult> Login(UserViewModel uvm)
+        {
+            var user = await _userManager.FindByNameAsync(uvm.emailaddress);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, uvm.password))
+            {
+                try
+                {
+
+                    var roles = await _userManager.GetRolesAsync(user); //Get roles
+                    var userRole = roles.FirstOrDefault(); //Get the users role
+
+                    var generatedToken = await GenerateJWTToken(user);
+                    return Ok(new
+                    {
+                        token = generatedToken,
+                        user = user.Email,
+                        role = userRole
+                    });
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception message
+                    Console.WriteLine($"Token generation failed: {ex.Message}");
+                    throw; // rethrow to be caught by outer try-catch
+                           // return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please contact support.");
+                }
+            }
+            else
+            {
+                return NotFound("Does not exist");
+            }
+        }
+
+        //Generate token
+
+        [HttpGet]
+        private async Task<ActionResult> GenerateJWTToken(AppUser user)
+        {
+            var role = await _userManager.GetRolesAsync(user);
+            IdentityOptions _identityOptions = new IdentityOptions();
+            // Create JWT Token
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+
+            };
+
+            if (role.Count() > 0)
+            {
+                claims.Add(new Claim(_identityOptions.ClaimsIdentity.RoleClaimType, role.FirstOrDefault()));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _configuration["Tokens:Issuer"],
+                _configuration["Tokens:Audience"],
+                claims,
+                signingCredentials: credentials,
+                expires: DateTime.UtcNow.AddHours(3)
+            );
+
+            return Created("", new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                user = user.UserName
+            });
+        }
+
+
+        //Create user role
+
+        [HttpPost]
+        [Route("CreateRole")]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            var role = await _roleManager.FindByNameAsync(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = roleName
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+
+                if (result.Errors.Count() > 0) return BadRequest(result.Errors);
+            }
+            else
+            {
+                return Forbid("Role already exists.");
+            }
+
+            return Ok();
+        }
+
+        //Assign user role
+
+        [HttpPost]
+        [Route("AssignRole")]
+        public async Task<IActionResult> AssignRole(string emailAddress, string roleName)
+        {
+            var user = await _userManager.FindByEmailAsync(emailAddress);
+            if (user == null) return NotFound();
+
+            var result = await _userManager.AddToRoleAsync(user, roleName);
+            if (result.Succeeded) return Ok();
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [Authorize(Roles = "Admin, Manager")]
+        [Route("RoleTest")]
+        public IActionResult RoleTest()
+        {
+            return Ok("You are an admin or manager!!!");
+        }
+
+
+
+    }
+}
---
+++ Assignment3_API/Controllers/Report.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assignment3_API.ViewModels;
+using Assignment3_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using static Assignment3_API.Controllers.Report;
+
+namespace Assignment3_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Report : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+        public Report(AppDbContext appDbContext)
+        { 
+            _context = appDbContext;
+        }
+
+
+        //Get products
+        [HttpGet("ProductListing")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.ProductType)
+                .Select(p => new
+                {
+                    p.ProductId,
+                    p.Name,
+                    p.Image,
+                    p.Description,
+                    p.Price,
+                    BrandName = p.Brand.Name,
+                    ProductTypeName = p.ProductType.Name,
+                })
+                .ToListAsync();
+
+            //Check if there are products in the database
+            if(products == null || products.Count == 0) 
+            { 
+                return NotFound("No products found"); 
+            }
+            return Ok(products);
+        }
+
+
+        public class ProductDto
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Image { get; set; }  // Base64 string or another image format
+            public int BrandId { get; set; }
+            public int ProductTypeId { get; set; }
+            public decimal Price { get; set; }
+        }
+
+
+        //Adding products
+        [HttpPost("AddProduct")]
+        public async Task<ActionResult<Product>> AddProduct( ProductDto productDto)
+        {
+            if (productDto == null)
+            {
+                return BadRequest("Product data is null.");
+            }
+
+            //Ensure the product type and brand exist
+            var brand = await _context.Brands.FindAsync(productDto.BrandId);
+            var productType = await _context.ProductTypes.FindAsync(productDto.ProductTypeId); ;
+
+            if (brand == null || productType == null)
+            {
+                return BadRequest("Invalid Brand or ProductType.");
+
+            }
+
+        
+
+            // Map DTO to Product entity
+            var product = new Product
+            {
+                Name = productDto.Name,
+                Description = productDto.Description,
+                BrandId = productDto.BrandId,
+                ProductTypeId = productDto.ProductTypeId,
+                Price = productDto.Price,
+                Image = productDto.Image,  // Handle image conversion if it's in base64 format
+            };
+
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Product created", product = new { id = product.ProductId, name = product.Name } });
+
+
+        }
+
+
+
+
+        //Get brands
+        [HttpGet("GetBrands")]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrand()
+        {
+            var brands = await _context.Brands.ToListAsync();
+            if(brands ==  null || brands.Count == 0)
+            {
+                return NotFound("No brands found.");
+            }
+
+            return Ok(brands);
+        }
+
+        //Get Product types
+        [HttpGet("GetProdutType")]
+        public async Task<ActionResult<IEnumerable<ProductType>>> GetProductType()
+        {
+            var productType = await _context.ProductTypes.ToListAsync();
+            if(productType == null || productType.Count == 0)
+            {
+                return NotFound("No product type found.");
+            }
+
+            return Ok(productType);
+        }
+
+
+        //Get Top 10 most expensive products
+        [HttpGet("TenMostExpensiveProducts")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetTop10ExpesniveProducts()
+        {
+            var products = await _context.Products
+                                         .Include(p => p.Brand)
+                                         .Include(p => p.ProductType)
+                                         .OrderByDescending(p => p.Price)
+                                         .Take(10)
+                                         .ToListAsync();
+
+            // Check if there are no products
+            if (products == null || products.Count == 0)
+            {
+                return NotFound("No products found.");
+            }
+
+
+            // Select the relevant fields to return
+            var result = products.Select(p => new
+            {
+                p.Name,
+                p.Price,
+                Brand = p.Brand.Name,
+                ProductType = p.ProductType.Name, 
+                p.Description
+            }).ToList();
+
+            return Ok(result);
+
+
+        }
+
+
+
+        //Grouped by brand
+        [HttpGet("Reports")]
+        public async Task<IActionResult> GetProductCountForDashboard()
+        {
+           //Brand count grouped by name
+           var brandcount = await _context.Products.
+                                                   Include(p => p.Brand)
+                                                   .GroupBy(p => p.Brand.Name)
+                                                   .Select(g => new
+                                                   {
+                                                       Label = g.Key,
+                                                       Count = g.Count()
+                                                   }).ToListAsync();
+
+            //Product Type count
+            var typeCount = await _context.Products.
+                                                    Include(p => p.ProductType)
+                                                    .GroupBy(p => p.ProductType.Name)
+                                                    .Select(g => new
+                                                    {
+                                                        Label = g.Key,
+                                                        Count = g.Count()
+                                                    }).ToListAsync();
+
+            return Ok(new
+            {
+                brandChart = brandcount,
+                typeChart = typeCount
+            });
+
+
+        }
+
+
+
+
+    }
+}
